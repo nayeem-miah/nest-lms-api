7b3ad88fb1d4600bfdc61a9e0eaf28ee290a130d
nayeem-miah <nayeem5113a@gmail.com>
2026-02-01 12:19:52 +0600

    reditect

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:3001',
       'http://localhost:5173',
       // 'https://your-frontend.vercel.app',
     ],
